5d96c4dd65eb42bc8cf53be33fcfa5393817e649
Manuel Vrsalovic <manuel.vrsalovic@gmail.com>
2022-09-15 22:12:58 -0400

    Contenido en paginas y edicion de estilos

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,8 +12,8 @@ configuraban las views y finalmente no se encontraba el archivo de vistas.
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("views", join(__dirname, "../views"));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => res.render("index"))
-app.get("/about", (req, res) => res.render("about"))
-app.get("/contact", (req, res) => res.render("contact"))
+app.get("/", (req, res) => res.render("index", { title: "NodeBrain" }))
+app.get("/about", (req, res) => res.render("about", { title: "About NodeBrain" }))
+app.get("/contact", (req, res) => res.render("contact", { title: "Contact NodeBrain" }))
 
 export default app
